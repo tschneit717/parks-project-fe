83a02a80b65883e6422a949d0f3ab372e97e3da8
Tom Schneider <tschneit717@gmail.com>
2023-01-14 19:59:15 -0500

    Clean slate

--- src/api/register.tsx
+++
@@ -1,9 +0,0 @@
-export const register = async (body: any) => {
-  await fetch("http://localhost:8080/user/register", {
-    method: "POST",
-    body,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-};
